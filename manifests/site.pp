0eb94607d473dc7510f987b880b27d500bb5fc9f
milena <milena@puppetlabs.vm>
2018-06-05 19:03:52 +0000

    Atualizar 'manifests/site.pp'

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,7 +43,7 @@ node default {
   # Example:
   #   class { 'my_class': }
   include role::classroom
-}
+
 
 file { '/etc/motd':
   ensure  => file,
@@ -56,3 +56,5 @@ file { '/etc/motd':
 exec { '/etc/motd':
         "cowsay 'Welcome to ${::fqdn}!' > /etc/motd",
      }
+     
+}     
