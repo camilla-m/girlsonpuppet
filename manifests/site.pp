c01187194a3a183794f13808bb99a834f5a11373
loiana <loiana@puppetlabs.vm>
2018-06-06 17:23:02 +0000

    Atualizar 'manifests/site.pp'

--- manifests/site.pp
+++ manifests/site.pp
@@ -45,7 +45,7 @@ node default {
   include role::classroom  
   include skeleton
 
-  if $facts['is_vitual'] == true {
+  if $facts['is_virtual'] == true {
     # notice("This is a virtual machine!\n")
     notify { "This is a virtual machine!\n": }
     
